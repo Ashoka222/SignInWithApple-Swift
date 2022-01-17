544e3cd7e91dcc54a777d030221ce802c01d1e1f
Ashok Kumbhar <ashok.kumbhar@mindbowser.com>
2022-01-17 10:52:42 +0530

    SignIn with Apple implemented

---
+++ SignInWithAppleDemo/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  SignInWithAppleDemo
+//
+//  Created by Apple on 31/03/21.
+//
+
+import UIKit
+import AuthenticationServices
+
+class ViewController: UIViewController, ASAuthorizationControllerDelegate {
+    
+    @IBOutlet weak var signInButtonStack: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setUpSignInAppleButton()
+    }
+    
+    func setUpSignInAppleButton() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAppleIdRequest), for: .touchUpInside)
+        authorizationButton.cornerRadius = 10
+        //Add button on some view or stack
+        self.signInButtonStack.addArrangedSubview(authorizationButton)
+    }
+    
+    @objc func handleAppleIdRequest() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()        
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as?  ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            print("User id is \(userIdentifier) \n Full Name is \(String(describing: fullName)) \n Email id is \(String(describing: email))")
+                        
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
+            appleIDProvider.getCredentialState(forUserID: userIdentifier) {  (credentialState, error) in
+                switch credentialState {
+                case .authorized:
+                    // The Apple ID credential is valid.
+                    break
+                case .revoked:
+                    // The Apple ID credential is revoked.
+                    break
+                case .notFound: break
+                // No credential was found, so show the sign-in UI.
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print(error)
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+           return self.view.window!
+    }
+}
